ab9258122257ce9e79491fb1f6b8d5873c3449f1
Breno <breno_fari@hotmail.com>
2021-01-20 22:09:56 -0300

    Acrescentado UIManager, AudioManager e Select, para troca de personagens

---
+++ Assets/_Scripts/SelectManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> imgPlayers;
+    private Button boy1, girl1, boy2, girl2;
+    private Image selectedPlayerImg;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        boy1 = GameObject.Find("Menino1").GetComponent<Button>();
+        girl1 = GameObject.Find("Menina1").GetComponent<Button>();
+        boy2 = GameObject.Find("Menino2").GetComponent<Button>();
+        girl2 = GameObject.Find("Menina2").GetComponent<Button>();
+        selectedPlayerImg = GameObject.Find("PlayerImage").GetComponent<Image>();
+
+        boy1.onClick.AddListener(ChangePlayer);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangePlayer()
+    {
+        selectedPlayerImg.sprite = imgPlayers[0];
+
+    }
+}
---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager instance;
+    private Button _playGame, _exitGame, _home, _okbtn;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        SceneManager.sceneLoaded += Load;
+    }
+
+    void Load(Scene scene, LoadSceneMode mode)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            _playGame = GameObject.Find("PlayBtn").GetComponent<Button>();
+            _exitGame = GameObject.Find("ExitBtn").GetComponent<Button>();
+
+            _playGame.onClick.AddListener(PlayGame);
+            _exitGame.onClick.AddListener(ExitGame);
+
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            _home = GameObject.Find("Home").GetComponent<Button>();
+            _okbtn = GameObject.Find("OkBtn").GetComponent<Button>();
+
+            _home.onClick.AddListener(Home);
+            _okbtn.onClick.AddListener(OkBtn);
+        }
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void Home()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void OkBtn()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
